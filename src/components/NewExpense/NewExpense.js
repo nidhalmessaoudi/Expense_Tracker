33d6aa1b954860b94113e67d7e79ee83f3863549
Nidhal Messaoudi <nidhalmessaoudi@hotmail.com>
2021-11-02 23:51:47 +0100

    Working with state and user inputs

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,13 @@
+import ExpenseForm from "./ExpenseForm";
+
+import "./NewExpense.css";
+
+function newExpense() {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
+}
+
+export default newExpense;
